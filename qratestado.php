3aa99ca3428698086f2943fde7fb5d94cbe04821
Funepu <triangulosul34@gmail.com>
2020-07-16 10:57:50 -0300

    pequenas manutenções

--- qratestado.php
+++ qratestado.php
@@ -28,19 +28,23 @@ $atestado_id = $_GET['a'];
 $codigo = $_GET['c'];
 
 $sql = "SELECT * FROM atestados a INNER JOIN pessoas b ON a.pessoa_id = b.pessoa_id WHERE atestado_id = $atestado_id and atendimento_id = $codigo";
-$result = pg_query($sql) or die("Atestado invalido!!!");
+$result = pg_query($sql);
 $rowa = pg_fetch_object($result);
+
+$nome = $rowa->nome;
+$dia = $rowa->partir_dia;
+$qtd = $rowa->qtd_dias;
 ?>
 <!DOCTYPE html>
-<html lang="pt-br" class="loading">
+<html lang="en" class="loading">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="tsul" content="tsul">
-    <meta name="keywords" content="tsul">
-    <meta name="author" content="TSUL">
+    <meta name="description" content="Apex admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Apex admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
     <title>FUNEPU | Atestados</title>
     <link rel="apple-touch-icon" sizes="60x60" href="app-assets/img/ico/apple-icon-60.png">
     <link rel="apple-touch-icon" sizes="76x76" href="app-assets/img/ico/apple-icon-76.png">
@@ -58,89 +62,69 @@ $rowa = pg_fetch_object($result);
     <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/prism.min.css">
     <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/chartist.min.css">
     <link rel="stylesheet" type="text/css" href="app-assets/css/app.css">
-    <link rel="stylesheet" type="text/css" href="app-assets/css/tsul.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="app-assets/vendors/css/pickadate/pickadate.css">
-    <script defer src="/your-path-to-fontawesome/js/all.js"></script>
-    <!--load all styles -->
-
-
-
 </head>
+
 <style>
-    hr {
-        color: #12A1A6;
-        background-color: #12A1A6;
-        margin-top: 0px;
-        margin-bottom: 0px;
-        height: 4px;
-        width: 200px;
-        margin-left: 0px;
-        border-top-width: 0px;
+    body {
+        background-image: url('app-assets/img/gallery/login.jpeg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+
+    .sh {
+        -moz-box-shadow: 0 0 10px;
+        -webkit-box-shadow: 0 0 10px;
+        box-shadow: 0 0 10px;
     }
 </style>
 
-<body class="pace-done" cz-shortcut-listen="true">
-    <!-- <div class="pace pace-inactive">
-        <div class="pace-progress" data-progress-text="100%" data-progress="99" style="transform: translate3d(100%, 0px, 0px);">
-            <div class="pace-progress-inner"></div>
-        </div>
-        <div class="pace-activity"></div>
-    </div> -->
-
-    <!-- <div class="wrapper"> -->
-    <?php //include('menu.php'); 
-    ?>
-    <?php include('header3.php'); ?>
-    <div class="main-panel">
-        <div class="">
-            <div class="content-wrapper">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <!-- <div class="card-header" style="display: flex;align-items: center;justify-content: space-between; background: #00777a"> -->
-
-                            <div class="card-header">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="row">
-                                            <div class="col-12">
-                                                <h4 class="card-title">
-                                                    <p style="color: #12A1A6;display:inline;font-size: 8pt;font-weight: bold;">
-                                                        » </p>Atestados
-                                                </h4>
+<body data-col="1-column" class=" 1-column  blank-page">
+    <div class="wrapper">
+        <div class="main-panel">
+            <div class="main-content">
+                <div class="content-wrapper">
+                    <section>
+                        <div class="container-fluid">
+                            <div class="row full-height-vh m-0">
+                                <div class="col-12 d-flex align-items-center justify-content-center">
+                                    <div class="card sh">
+                                        <div class="card-content">
+                                            <div class="card-body login-img">
+                                                <div class="row m-0">
+                                                    <div class="col-lg-6 d-lg-block d-none py-2 text-center align-middle">
+                                                        <img src="app-assets/img/gallery/upa24.png" class="mt-3" alt="" width="280" height="180">
+                                                    </div>
+                                                    <div class="col-lg-6 col-md-12 bg-white px-4 pt-3">
+                                                        <form name='formulario' method='POST' action='checklogin.php'>
+                                                            <h4 class="mb-3 card-title"><strong>Verificação de atestado</strong></h4>
+                                                            <p><b>Paciente:</b> <?php echo $nome == '' ? '<span style="color:red">Não encontrado</span>' : iniciais($nome); ?></p>
+                                                            <p><b>Data:</b> <?php echo $dia == '' ? '<span style="color:red">Não encontrado</span>' : inverteData($dia); ?></p>
+                                                            <p><b>Dias de Atestado:</b> <?php echo $dia == '' ? '<span style="color:red">Não encontrado</span>' : $qtd; ?></p>
+                                                            <div class="row">
+                                                                <div class='col-md-6' align='center'>
+                                                                    <?php if ($nome == '') { ?>
+                                                                        <i class="fas fa-times-circle font-large-1" style="color: red;"></i>
+                                                                    <?php } else { ?>
+                                                                        <i class="fas fa-check-circle font-large-1" style="color: #15AABF;"></i>
+                                                                    <?php } ?>
+                                                                </div>
+                                                                <div class='col-md-6' align='right'><img src="app-assets/img/gallery/logotc.png" alt="" width="40" height="40"></div>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div class="col-12">
-                                                <hr>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-content">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col">
-
-
-                                            <p>Paciente: <?php echo iniciais($rowa->nome); ?></p>
-                                            <p>Data: <?php echo inverteData($rowa->partir_dia); ?></p>
-                                            <p>Dias de Atestado: <?php echo $rowa->qtd_dias; ?></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </section>
                 </div>
             </div>
         </div>
     </div>
-    <?php //include('footer.php'); 
-    ?>
-    <!-- </div> -->
-
     <script src="app-assets/vendors/js/core/jquery-3.2.1.min.js" type="text/javascript"></script>
     <script src="app-assets/vendors/js/core/popper.min.js" type="text/javascript"></script>
     <script src="app-assets/vendors/js/core/bootstrap.min.js" type="text/javascript"></script>
@@ -149,18 +133,9 @@ $rowa = pg_fetch_object($result);
     <script src="app-assets/vendors/js/jquery.matchHeight-min.js" type="text/javascript"></script>
     <script src="app-assets/vendors/js/screenfull.min.js" type="text/javascript"></script>
     <script src="app-assets/vendors/js/pace/pace.min.js" type="text/javascript"></script>
-    <script src="app-assets/vendors/js/chartist.min.js" type="text/javascript"></script>
     <script src="app-assets/js/app-sidebar.js" type="text/javascript"></script>
     <script src="app-assets/js/notification-sidebar.js" type="text/javascript"></script>
     <script src="app-assets/js/customizer.js" type="text/javascript"></script>
-    <script src="app-assets/js/dashboard1.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js" type="text/javascript"></script>
-    <script src="app-assets/js/scripts.js" type="text/javascript"></script>
-    <script src="app-assets/js/popover.js" type="text/javascript"></script>
-    <script src="app-assets/js/pick-a-datetime.js" type="text/javascript"></script>
-    <script defer src="/your-path-to-fontawesome/js/all.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </body>
 
 </html>
