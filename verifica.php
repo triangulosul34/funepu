bcd97da57e052cf81d5ab861b3f6dd84ded59549
Funepu <triangulosul34@gmail.com>
2020-08-04 16:01:16 -0300

    manutenção

--- verifica.php
+++ verifica.php
@@ -4,7 +4,7 @@
 
 session_start();
 
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1200)) {
     // last request was more than 30 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
