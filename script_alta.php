0fe6f381c4bbed964c418089e9f1af54b4e96d18
Funepu <triangulosul34@gmail.com>
2020-12-15 11:20:18 -0300

    manutenção

--- script_alta.php
+++ script_alta.php
@@ -1,4 +1,5 @@
 <?php
+
 // include("conexao.php");
 // $sql = "SELECT a.transacao, max((c.data || ' ' || c.hora)::timestamp) as data FROM atendimentos a
 // LEFT JOIN destino_paciente b ON a.transacao = b.atendimento_id
@@ -33,7 +34,7 @@
 //     $result2 = pg_query($sql2) or die($sql2);
 // }
 
-include("conexao.php");
+include 'conexao.php';
 $sql = "select a.transacao, data_destino, hora_destino, z.data data_enc, z.hora hora_enc, max((c.data || ' ' || c.hora)::timestamp) as data 
 from atendimentos a 
 left join destino_paciente z on a.transacao = z.atendimento_id 
@@ -45,24 +46,23 @@ then data_destino < NOW() - interval '2 day'
 else z.data < NOW() - interval '2 day' end 
 else c.data < NOW() - interval '2 day' end 
 and case when z.destino_encaminhamento::varchar is null then a.destino_paciente in ('03','07','10') else z.destino_encaminhamento::varchar in ('03','07','10') end
-group by 1,2,3,4,5
-order by dat_cad";
+group by 1,2,3,4,5";
 $result = pg_query($sql) or die($sql);
 while ($row = pg_fetch_object($result)) {
-    if ($row->data_evolucao == null) {
-        $data = $row->data_enc == null ? $row->data_destino : $row->data_enc;
-        $hora = $row->hora_enc == null ? $row->hora_destino : $row->hora_enc;
-    } else {
-        $data = substr($row->data, 0, 9);
-        $hora = substr($row->hora, 11);
-    }
-    
-    if ($row->data_enc == null) {
-        $sql2 = "insert into destino_paciente (atendimento_id, destino_encaminhamento, motivo,data, hora)
+	if ($row->data_evolucao == null) {
+		$data = $row->data_enc == null ? $row->data_destino : $row->data_enc;
+		$hora = $row->hora_enc == null ? $row->hora_destino : $row->hora_enc;
+	} else {
+		$data = substr($row->data, 0, 9);
+		$hora = substr($row->hora, 11);
+	}
+
+	if ($row->data_enc == null) {
+		$sql2 = "insert into destino_paciente (atendimento_id, destino_encaminhamento, motivo,data, hora)
         values ($row->transacao, 20, 'Alta automatizado via sistema', '" . $data . "', '" . $hora . "')";
-        $result2 = pg_query($sql2) or die($sql2);
-    } else {
-        $sql2 = "update destino_paciente set destino_encaminhamento = 20, motivo= 'Alta automatizado via sistema', data = '" . $data . "', hora = '" . $hora . "' where atendimento_id = $row->transacao";
-        $result2 = pg_query($sql2) or die($sql2);
-    }
+		$result2 = pg_query($sql2) or die($sql2);
+	} else {
+		$sql2 = "update destino_paciente set destino_encaminhamento = 20, motivo= 'Alta automatizado via sistema', data = '" . $data . "', hora = '" . $hora . "' where atendimento_id = $row->transacao";
+		$result2 = pg_query($sql2) or die($sql2);
+	}
 }
