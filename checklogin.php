99484864359ca812b94c7e3905062095ee28ee3a
Funepu <triangulosul34@gmail.com>
2021-02-23 14:19:06 -0300

    manutenção

--- checklogin.php
+++ checklogin.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'Config.php';
+
 require 'tsul_ssl.php';
 
 error_reporting(0);
@@ -28,28 +30,58 @@ $mypassword = pg_escape_string($mypassword);
 $sql = "SELECT * FROM pessoas WHERE username='$myusername' and password='$mypassword'";
 $result = pg_query($con, $sql) or die($sql);
 $row = pg_fetch_array($result);
-
-if ($row['username'] != '' and $row['perfil'] != '08' and $row['perfil'] != '03') {
-	session_start();
-	$_SESSION['myusername'] = $myusername;
-	$_SESSION['nome'] = ts_decodifica($row['nome']);
-	$_SESSION['mypassword'] = $mypassword;
-	$_SESSION['id'] = $row['pessoa_id'];
-	$_SESSION['perfil'] = $row['perfil'];
-	$_SESSION['imagem'] = $row['imagem'];
-	$_SESSION['box'] = $box;
-	$_SESSION['tipopessoa'] = $row['tipo_pessoa'];
-	$_SESSION['grupo'] = $row['sgrupo_user_id'];
-	$_SESSION['unidade'] = $unidade;
-	$data = date('Y-m-d');
-	$id = $row['pessoa_id'];
-	header('location:index.php');
-} elseif ($row['perfil'] == '08' or $row['perfil'] == '03') {
-	header('location: loginbox.php');
-} else {
-	if ($box == '') {
-		header('location:login.html');
+if (CONTROLE_ACESSO) {
+	if (($_SERVER['REMOTE_ADDR'] == '179.104.42.235' or $_SERVER['REMOTE_ADDR'] == '189.41.99.44') and ($row['pessoa_id'] == USUARIO[0] or $row['pessoa_id'] == USUARIO[1] or $row['pessoa_id'] == USUARIO[2] or $row['pessoa_id'] == USUARIO[3] or $row['pessoa_id'] == USUARIO[4] or $row['pessoa_id'] == USUARIO[5] or $row['pessoa_id'] == USUARIO[6] or $row['pessoa_id'] == USUARIO[7] or $row['pessoa_id'] == USUARIO[8])) {
+		if ($row['username'] != '' and $row['perfil'] != '08' and $row['perfil'] != '03') {
+			session_start();
+			$_SESSION['myusername'] = $myusername;
+			$_SESSION['nome'] = ts_decodifica($row['nome']);
+			$_SESSION['mypassword'] = $mypassword;
+			$_SESSION['id'] = $row['pessoa_id'];
+			$_SESSION['perfil'] = $row['perfil'];
+			$_SESSION['imagem'] = $row['imagem'];
+			$_SESSION['box'] = $box;
+			$_SESSION['tipopessoa'] = $row['tipo_pessoa'];
+			$_SESSION['grupo'] = $row['sgrupo_user_id'];
+			$_SESSION['unidade'] = $unidade;
+			$data = date('Y-m-d');
+			$id = $row['pessoa_id'];
+			header('location:index.php');
+		} elseif ($row['perfil'] == '08' or $row['perfil'] == '03') {
+			header('location: loginbox.php');
+		} else {
+			if ($box == '') {
+				header('location:login.html');
+			} else {
+				header('location:loginbox.html');
+			}
+		}
 	} else {
-		header('location:loginbox.html');
+		echo "<script>alert('Acesso não permitido por esse dispositivo!!!');window.location.href = 'http://www.google.com.br';</script>";
+	}
+} else {
+	if ($row['username'] != '' and $row['perfil'] != '08' and $row['perfil'] != '03') {
+		session_start();
+		$_SESSION['myusername'] = $myusername;
+		$_SESSION['nome'] = ts_decodifica($row['nome']);
+		$_SESSION['mypassword'] = $mypassword;
+		$_SESSION['id'] = $row['pessoa_id'];
+		$_SESSION['perfil'] = $row['perfil'];
+		$_SESSION['imagem'] = $row['imagem'];
+		$_SESSION['box'] = $box;
+		$_SESSION['tipopessoa'] = $row['tipo_pessoa'];
+		$_SESSION['grupo'] = $row['sgrupo_user_id'];
+		$_SESSION['unidade'] = $unidade;
+		$data = date('Y-m-d');
+		$id = $row['pessoa_id'];
+		header('location:index.php');
+	} elseif ($row['perfil'] == '08' or $row['perfil'] == '03') {
+		header('location: loginbox.php');
+	} else {
+		if ($box == '') {
+			header('location:login.html');
+		} else {
+			header('location:loginbox.html');
+		}
 	}
 }
--- checkloginbox.php
+++ checkloginbox.php
@@ -1,36 +1,105 @@
 <?php
 
-//if ($_SERVER['REMOTE_ADDR'] == '179.104.42.235' or $_SERVER['REMOTE_ADDR'] == '189.41.99.44') {
-	error_reporting(0);
+include 'Config.php';
 
-	include 'conexao.php';
-	include 'tsul_ssl.php';
+error_reporting(0);
 
-	$myusername = $_POST['myusername'];
-	$mypassword = md5($_POST['mypassword']);
-	$box = $_POST['box'];
+include 'conexao.php';
+include 'tsul_ssl.php';
 
-	$myusername = stripslashes($myusername);
-	$mypassword = stripslashes($mypassword);
+$myusername = $_POST['myusername'];
+$mypassword = md5($_POST['mypassword']);
+$box = $_POST['box'];
 
-	$unidade = '';
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
 
-	$ip = getenv('REMOTE_ADDR');
-	if ($ip = '201.48.4.1') {
-		$unidade == '1';
-		$unid_desc = 'Mirante';
-	} elseif ($ip = '201.48.4.2') {
-		$unidade == '2';
-		$unid_desc = 'São Benedito';
+$unidade = '';
+
+$ip = getenv('REMOTE_ADDR');
+if ($ip = '201.48.4.1') {
+	$unidade == '1';
+	$unid_desc = 'Mirante';
+} elseif ($ip = '201.48.4.2') {
+	$unidade == '2';
+	$unid_desc = 'São Benedito';
+}
+
+$myusername = pg_escape_string($myusername);
+$mypassword = pg_escape_string($mypassword);
+
+$sql = "SELECT * FROM pessoas WHERE username='$myusername' and password='$mypassword'";
+$result = pg_query($con, $sql) or die($sql);
+$row = pg_fetch_array($result);
+if (CONTROLE_ACESSO) {
+	if (($_SERVER['REMOTE_ADDR'] == '179.104.42.235' or $_SERVER['REMOTE_ADDR'] == '189.41.99.44') and ($row['pessoa_id'] == USUARIO[0] or $row['pessoa_id'] == USUARIO[1] or $row['pessoa_id'] == USUARIO[2] or $row['pessoa_id'] == USUARIO[3] or $row['pessoa_id'] == USUARIO[4] or $row['pessoa_id'] == USUARIO[5] or $row['pessoa_id'] == USUARIO[6] or $row['pessoa_id'] == USUARIO[7] or $row['pessoa_id'] == USUARIO[8])) {
+		//if ($box == $_POST['conf_consultorio'] && $box != '') {
+		if ($row['username'] != '') {
+			session_start();
+			$_SESSION['myusername'] = $myusername;
+			$_SESSION['nome'] = ts_decodifica($row['nome']);
+			$_SESSION['mypassword'] = $mypassword;
+			$_SESSION['id'] = $row['pessoa_id'];
+			$_SESSION['perfil'] = $row['perfil'];
+			$_SESSION['imagem'] = $row['imagem'];
+			$_SESSION['box'] = $box;
+			$id = $row['pessoa_id'];
+			$_SESSION['unidade'] = $unidade;
+
+			if (($_SESSION['perfil'] == '03' and $_SESSION['box'] == '8') or ($_SESSION['perfil'] == '03' and $_SESSION['box'] == '9')) {
+				$_SESSION['perfil'] = '08';
+				$aux = 1;
+			}
+
+			include 'conexao.php';
+			$data_hora = date('Y-m-d H:i:s');
+			$sql = "insert into logons (data_hora, usuario, consultorio) values ('$data_hora', '$myusername', '$box') ";
+			$result = pg_query($con, $sql) or die($sql);
+			$row = pg_fetch_array($result);
+
+			//validaçao de acesso
+			// if ($box == '20' or $box == '21') {
+			// 	header('location:index.php');
+			// } elseif ($box == '11') {
+			// 	header('location:painel_at_ala_vermelha.php');
+			// } else
+			if ($_SESSION['perfil'] == '03') {
+				if ($box >= 1 and $box <= 5) {
+					header('location:monitor_medico.php');
+				} elseif ($box == 6) {
+					header('location:painel_at_ortopedia.php');
+				} elseif ($box == 7) {
+					header('location:painel_at_ala_vermelha.php');
+				} else {
+					header('location:monitor_medico.php');
+				}
+			} elseif ($_SESSION['perfil'] == '08') {
+				if ($aux == 1) {
+					header('location:triagemRecepcao.php');
+				} else {
+					header('location:monitor_triagem.php');
+				}
+			} elseif ($_SESSION['perfil'] == '09') {
+				header('location:painel_at_ortopedia.php');
+			} elseif ($_SESSION['perfil'] == '05') {
+				header('location:painel_rx.php');
+			} else {
+				header('location:index.php');
+			}
+		} else {
+			if ($box == '') {
+				//('location:login.html');
+			} else {
+				//header('location:loginbox.php');
+			}
+		}
+		// } else {
+// 	header('location:loginbox.php');
+// }
+	} else {
+		echo "<script>alert('Acesso não permitido por esse dispositivo!!!');window.location.href = 'http://www.google.com.br';</script>";
 	}
-
-	$myusername = pg_escape_string($myusername);
-	$mypassword = pg_escape_string($mypassword);
-
-	$sql = "SELECT * FROM pessoas WHERE username='$myusername' and password='$mypassword'";
-	$result = pg_query($con, $sql) or die($sql);
-	$row = pg_fetch_array($result);
-
+} else {
 	//if ($box == $_POST['conf_consultorio'] && $box != '') {
 	if ($row['username'] != '') {
 		session_start();
@@ -94,6 +163,4 @@
 	// } else {
 // 	header('location:loginbox.php');
 // }
-//} else {
-	//echo "<script>alert('Acesso não permitido por esse dispositivo!!!');window.location.href = 'http://www.google.com.br';</script>";
-//}
+}
